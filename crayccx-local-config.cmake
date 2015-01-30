1e518e6ac8afb319be8fa01240f3ca2aafde9d85
Tiago Quintino <tiago.quintino@ecmwf.int>
2015-01-30 21:08:52 +0000

    Cray config

---
+++ crayccx-local-config.cmake
@@ -0,0 +1,19 @@
+# general
+
+set( CMAKE_C_COMPILER_INIT        cc )
+set( CMAKE_CXX_COMPILER_INIT      CC )
+set( CMAKE_Fortran_COMPILER_INIT  ftn )
+
+set( CMAKE_INSTALL_PREFIX "$ENV{HOME}/data/install/MarsClient" )
+
+# grib_api
+set( GRIB_API_SHARE "/usr/local/apps/grib_api/1.12.3/GNU/48/share/grib_api" )
+set( GRIB_API_DEFINITION_PATH "${GRIB_API_SHARE}/definitions" )
+set( GRIB_API_SAMPLES_PATH    "${GRIB_API_SHARE}/samples" )
+
+# fdb
+set( BUILD_IFSIO OFF )
+set( FDB_CONF_FILE "/usr/local/apps/fdb/etc/Config" )
+
+# libemos
+set( ENABLE_INSTALL_TABLES OFF )
