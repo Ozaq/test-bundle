b3e8aa206f627d9b3b035bfe94529ec88d848c01
Tiago Quintino <tiago.quintino@ecmwf.int>
2015-01-30 22:55:08 +0000

    lxab works

--- lxab-local-config.cmake
+++ lxab-local-config.cmake
@@ -6,9 +6,11 @@ set( CMAKE_Fortran_COMPILER_INIT  gfortran )
 
 set( CMAKE_INSTALL_PREFIX "/var/tmp/matq/install/MarsClient" )
 
+set( ENABLE_TESTS OFF CACHE INTERNAL "" )
+
 # grib_api
 # needed only to run from build tree
-set( GRIB_API_SHARE "/usr/local/apps/grib_api/1.12.3" )
+set( GRIB_API_SHARE "/usr/local/apps/grib_api/1.12.3/GNU/4.8.1/share/grib_api" )
 set( GRIB_API_DEFINITION_PATH "${GRIB_API_SHARE}/definitions" )
 set( GRIB_API_SAMPLES_PATH    "${GRIB_API_SHARE}/samples" )
 
