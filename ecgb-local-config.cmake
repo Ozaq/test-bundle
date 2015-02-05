73428d75c67712dc1c9115f695649d224f429de2
Tiago Quintino <tiago.quintino@ecmwf.int>
2015-02-05 16:09:09 +0000

    Create common configuration

--- ecgb-local-config.cmake
+++ ecgb-local-config.cmake
@@ -1,20 +1,12 @@
+include( ecmwf-local-config.cmake OPTIONAL )
+
 # general
 
 set( CMAKE_C_COMPILER_INIT        gcc )
 set( CMAKE_CXX_COMPILER_INIT      g++ )
 set( CMAKE_Fortran_COMPILER_INIT  gfortran )
 
-set( CMAKE_INSTALL_PREFIX "/var/tmp/matq/install/MarsClient" )
-
 # grib_api
-# needed only to run from build tree
 set( GRIB_API_SHARE "/usr/local/apps/grib_api/1.12.3" )
 set( GRIB_API_DEFINITION_PATH "${GRIB_API_SHARE}/definitions" )
 set( GRIB_API_SAMPLES_PATH    "${GRIB_API_SHARE}/samples" )
-
-# fdb
-set( BUILD_IFSIO OFF )
-set( FDB_CONF_FILE "/usr/local/apps/fdb/etc/Config" )
-
-# libemos
-set( ENABLE_INSTALL_TABLES OFF CACHE INTERNAL "" )
--- opensuse113-local-config.cmake
+++ opensuse113-local-config.cmake
@@ -1,24 +1,17 @@
-# general
+include( ecmwf-local-config.cmake OPTIONAL )
 
-set( CMAKE_BUILD_TYPE             Production )
+# general
 
 set( CMAKE_C_COMPILER_INIT        gcc )
 set( CMAKE_CXX_COMPILER_INIT      g++ )
 set( CMAKE_Fortran_COMPILER_INIT  pgf90 )
-
-set( CMAKE_INSTALL_PREFIX "/tmp/install/MarsClient" )
 
 # grib_api
 set( GRIB_API_SHARE "/usr/local/apps/grib_api/1.13.0/GNU/4.8.1/share/grib_api" )
 set( GRIB_API_DEFINITION_PATH "${GRIB_API_SHARE}/definitions" )
 set( GRIB_API_SAMPLES_PATH    "${GRIB_API_SHARE}/samples" )
 
-# fdb
-set( BUILD_IFSIO OFF )
-set( FDB_CONF_FILE "/usr/local/apps/fdb/etc/Config" )
-
 # libemos
-set( ENABLE_INSTALL_TABLES OFF CACHE INTERNAL "" )
 set( ENABLE_FORTRAN90 OFF CACHE INTERNAL "" )
 set( ENABLE_FORTRAN   OFF CACHE INTERNAL "" )
 set( WITH_PGI_FORTRAN ON  CACHE INTERNAL "" )
