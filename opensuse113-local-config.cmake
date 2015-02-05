8a29ec43fb18c814aca12a98c3091aaf0ec93854
max <mars@ecmwf.int>
2015-02-05 16:56:55 +0000

    use correct path in opensuse113

--- opensuse113-local-config.cmake
+++ opensuse113-local-config.cmake
@@ -4,10 +4,11 @@ include( ecmwf-local-config.cmake OPTIONAL )
 
 set( CMAKE_C_COMPILER_INIT        gcc )
 set( CMAKE_CXX_COMPILER_INIT      g++ )
-set( CMAKE_Fortran_COMPILER_INIT  pgf90 )
+# set( CMAKE_Fortran_COMPILER_INIT  pgf90 )
+set( CMAKE_Fortran_COMPILER_INIT  gfortran )
 
 # grib_api
-set( GRIB_API_SHARE "/usr/local/apps/grib_api/1.13.0/GNU/4.8.1/share/grib_api" )
+set( GRIB_API_SHARE "/usr/local/lib/metaps/lib/grib_api/1.13.0/"
 set( GRIB_API_DEFINITION_PATH "${GRIB_API_SHARE}/definitions" )
 set( GRIB_API_SAMPLES_PATH    "${GRIB_API_SHARE}/samples" )
 
