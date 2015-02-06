c865e0c37957e9b5f29227f74ffc4cabc0e03d8d
max <mars@ecmwf.int>
2015-02-06 15:57:11 +0000

    fix for grib on ecgb

--- ecgb-local-config.cmake
+++ ecgb-local-config.cmake
@@ -7,6 +7,6 @@ set( CMAKE_CXX_COMPILER_INIT      g++ )
 set( CMAKE_Fortran_COMPILER_INIT  gfortran )
 
 # grib_api
-set( GRIB_API_SHARE "/usr/local/apps/grib_api/1.12.3" )
+set( GRIB_API_SHARE "/usr/local/apps/grib_api/1.12.3/share/grib_api" )
 set( GRIB_API_DEFINITION_PATH "${GRIB_API_SHARE}/definitions" )
 set( GRIB_API_SAMPLES_PATH    "${GRIB_API_SHARE}/samples" )
