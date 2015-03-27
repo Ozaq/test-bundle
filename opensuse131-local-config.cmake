54b345d04b845136cdd7136355e50961cfc2484e
Tiago Quintino <tiago.quintino@ecmwf.int>
2015-03-27 12:21:22 +0000

    settings on opensuse131

--- opensuse131-local-config.cmake
+++ opensuse131-local-config.cmake
@@ -6,3 +6,5 @@ set( CMAKE_C_COMPILER_INIT        gcc )
 set( CMAKE_CXX_COMPILER_INIT      g++ )
 set( CMAKE_Fortran_COMPILER_INIT  gfortran )
 
+set( EIGEN3_PATH "/usr/local/apps/eigen/3.2.0/" )
+set( CGAL_PATH   "/usr/local/apps/cgal/3.5.0/GNU/4.8.1/" )
