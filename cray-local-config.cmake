bdf64d5f3aa3d4225f9a16f81e6806b79ba2f44f
Tiago Quintino <tiago.quintino@ecmwf.int>
2015-03-27 14:21:43 +0000

    use grib_api from inside

--- cray-local-config.cmake
+++ cray-local-config.cmake
@@ -5,8 +5,3 @@ include( ecmwf-local-config.cmake OPTIONAL )
 set( CMAKE_C_COMPILER_INIT        cc )
 set( CMAKE_CXX_COMPILER_INIT      CC )
 set( CMAKE_Fortran_COMPILER_INIT  ftn )
-
-# grib_api
-set( GRIB_API_SHARE "/usr/local/apps/grib_api/1.12.3/GNU/48/share/grib_api" )
-set( GRIB_API_DEFINITION_PATH "${GRIB_API_SHARE}/definitions" )
-set( GRIB_API_SAMPLES_PATH    "${GRIB_API_SHARE}/samples" )
--- opensuse113-local-config.cmake
+++ opensuse113-local-config.cmake
@@ -7,11 +7,6 @@ set( CMAKE_CXX_COMPILER_INIT      g++ )
 # set( CMAKE_Fortran_COMPILER_INIT  pgf90 )
 set( CMAKE_Fortran_COMPILER_INIT  gfortran )
 
-# grib_api
-set( GRIB_API_SHARE "/usr/local/lib/metaps/lib/grib_api/1.13.0/share/grib_api" )
-set( GRIB_API_DEFINITION_PATH "${GRIB_API_SHARE}/definitions" )
-set( GRIB_API_SAMPLES_PATH    "${GRIB_API_SHARE}/samples" )
-
 # libemos
 set( ENABLE_FORTRAN90 OFF CACHE INTERNAL "" )
 set( ENABLE_FORTRAN   OFF CACHE INTERNAL "" )
