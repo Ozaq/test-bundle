71345ce3e7e9501b26b41c3c847bd7da7808be2d
Tiago Quintino <tiago.quintino@ecmwf.int>
2015-06-09 11:29:34 +0100

    remove useless config files

--- cray-local-config.cmake
+++
@@ -1,7 +0,0 @@
-include( ecmwf-local-config.cmake OPTIONAL )
-
-# general
-
-set( CMAKE_C_COMPILER_INIT        cc )
-set( CMAKE_CXX_COMPILER_INIT      CC )
-set( CMAKE_Fortran_COMPILER_INIT  ftn )
--- opensuse131-local-config.cmake
+++
@@ -1,7 +0,0 @@
-include( ecmwf-local-config.cmake OPTIONAL )
-
-# general
-
-set( CMAKE_C_COMPILER_INIT        gcc )
-set( CMAKE_CXX_COMPILER_INIT      g++ )
-set( CMAKE_Fortran_COMPILER_INIT  gfortran )
