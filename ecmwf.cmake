f1d604dc903685cdb45e8eb79bf4f74c4b7f7b1f
Tiago Quintino <tiago.quintino@ecmwf.int>
2015-11-16 17:34:45 +0000

    Fix config for ECMWF

--- ecmwf.cmake
+++ ecmwf.cmake
@@ -1,3 +1,5 @@
+option( ENABLE_PYTHON OFF )
+
 ### FDB
 
 option( ENABLE_FDB ON )
@@ -5,6 +7,8 @@ option( ENABLE_FDB ON )
 set( FDB_CONF_FILE "/usr/local/apps/fdb/etc/Config" )
 
 ### BUFR
+
+option( ENABLE_SINGLE_PRECISION OFF )
 
 set( BUFR_TABLES_PATH "/usr/local/apps/libemos/tables/bufr/text/000405" )
 
