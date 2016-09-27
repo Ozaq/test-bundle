0cc83bf9bc553dc4e3877cf6f16fef4945acf75c
Tiago Quintino <tiago.quintino@ecmwf.int>
2016-09-27 16:53:22 +0100

    MEMFS opn

--- ecmwf-local-config.cmake
+++ ecmwf-local-config.cmake
@@ -11,3 +11,5 @@ set( FDB_CONF_FILE "/usr/local/apps/fdb/etc/Config" )
 # libemos
 set( ENABLE_INSTALL_TABLES OFF CACHE INTERNAL "" )
 
+set( ENABLE_MEMFS  ON CACHE BOOL "activate memfs in eccodes" FORCE )
+
