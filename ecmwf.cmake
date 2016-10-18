9c90d44fb3a037dbedca7582c801e22916b0671c
Tiago Quintino <tiago.quintino@ecmwf.int>
2016-10-18 09:05:29 +0100

    Fix cache forced so user in comand line can override

--- ecmwf.cmake
+++ ecmwf.cmake
@@ -5,11 +5,11 @@ endmacro()
 
 ### PYTHON
 
-ecbuild_set_verbose( ENABLE_PYTHON OFF CACHE BOOL "Python disabled" FORCE )
+ecbuild_set_verbose( ENABLE_PYTHON OFF CACHE BOOL "Python disabled" )
 
 ### FDB
 
-ecbuild_set_verbose( ENABLE_FDB ON CACHE BOOL "FDB enabled" FORCE )
+ecbuild_set_verbose( ENABLE_FDB ON CACHE BOOL "FDB enabled" )
 
 ecbuild_set_verbose( FDB_CONF_FILE "/usr/local/apps/fdb/etc/Config" )
 
