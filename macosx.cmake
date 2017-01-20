4ccd9d484c5c8e9531af0d6424175f9a830cb20f
Tiago Quintino <tiago.quintino@ecmwf.int>
2017-01-20 19:30:11 +0000

    Add other configs

--- macosx.cmake
+++ macosx.cmake
@@ -5,11 +5,8 @@ ecbuild_set_verbose( ENABLE_PYTHON OFF CACHE BOOL "Python disabled" )
 ### FDB
 
 ecbuild_set_verbose( ENABLE_FDB ON CACHE BOOL "FDB enabled" )
-
-ecbuild_set_verbose( FDB_CONF_FILE "/usr/local/apps/fdb/etc/Config" )
+# ecbuild_set_verbose( FDB_CONF_FILE "/usr/local/apps/fdb/etc/Config" )
 
 ### BUFR
 
 ecbuild_set_verbose( ENABLE_SINGLE_PRECISION OFF )
-
-ecbuild_set_verbose( BUFR_TABLES_PATH "/usr/local/apps/libemos/tables/bufr/text/000408" )
