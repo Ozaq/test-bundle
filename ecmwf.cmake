64537bf297b9fcce5e2b3d19f428c9ab66ddb81b
Sebastien Villaume <sebastien.villaume@ecmwf.int>
2017-03-08 13:16:06 +0000

    enables a client with FDB4 and FDB5 support

--- ecmwf.cmake
+++ ecmwf.cmake
@@ -6,7 +6,8 @@ ecbuild_set_verbose( ENABLE_PYTHON OFF CACHE BOOL "Python disabled" )
 
 ecbuild_set_verbose( ENABLE_MARS_SERVER OFF CACHE BOOL "Don't build the server, just the libraries" )
 ecbuild_set_verbose( ENABLE_POINTDB     OFF CACHE BOOL "Don't build the pointdb server, just the libraries" )
-ecbuild_set_verbose( ENABLE_FDB         ON  CACHE BOOL "Both FDB4 and 5 enabled" )
+ecbuild_set_verbose( ENABLE_FDB         ON  CACHE BOOL "Both for FDB4 (client) and FDB5 (server)" )
+ecbuild_set_verbose( ENABLE_FDB5        ON  CACHE BOOL "FDB5 enabled in client" )
 
 ecbuild_set_verbose( FDB_CONF_FILE "/usr/local/apps/fdb/etc/Config" )
 
