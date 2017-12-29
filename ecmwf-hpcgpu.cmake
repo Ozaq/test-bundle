dafb1b681ff3fabf951336034dfdc77f549943f1
Max <max@ecmwf.int>
2017-12-29 16:13:39 +0000

    Activate ECMWF

--- ecmwf-hpcgpu.cmake
+++ ecmwf-hpcgpu.cmake
@@ -1,3 +1,4 @@
+ecbuild_set_verbose( ECMWF  ON  CACHE BOOL "ECMWF enabled" )
 ecbuild_set_verbose( ENABLE_MEMFS  ON  CACHE BOOL "memfs enabled" )
 ecbuild_set_verbose( ENABLE_PYTHON OFF CACHE BOOL "Python disabled" )
 
