b6c19f86826c91b5d8f058a0f4f515ed2c78d931
Tiago Quintino <tiago.quintino@ecmwf.int>
2020-03-26 16:44:44 +0000

    Cleanup configs

--- ecmwf.cmake
+++ ecmwf.cmake
@@ -10,8 +10,7 @@ ecbuild_set_verbose( ENABLE_SINGLE_PRECISION OFF )
 
 ecbuild_set_verbose( BUFR_TABLES_PATH "/usr/local/apps/libemos/tables/bufr/text/000408" )
 
-### FDB
-
+ecbuild_set_verbose( ENABLE_ODB2        ON  CACHE BOOL "ODB2 in metkit" )
 ecbuild_set_verbose( ENABLE_FDB         ON  CACHE BOOL "Both for FDB4 (client) and FDB5 (server)" )
 ecbuild_set_verbose( ENABLE_MIR_TOOLS   ON  CACHE BOOL "Install MIR tools" )
 
