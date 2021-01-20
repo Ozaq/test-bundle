e308697b4f42a62d280609e0a94f2506dee21d38
Tiago Quintino <tiago.quintino@ecmwf.int>
2021-01-20 09:26:51 +0000

    Remove old tables

--- ecmwf.cmake
+++ ecmwf.cmake
@@ -8,8 +8,6 @@ ecbuild_set_verbose( ENABLE_PYTHON OFF CACHE BOOL "Python disabled" )
 
 ecbuild_set_verbose( ENABLE_SINGLE_PRECISION OFF )
 
-ecbuild_set_verbose( BUFR_TABLES_PATH "/usr/local/apps/libemos/tables/bufr/text/000408" )
-
 ecbuild_set_verbose( ENABLE_ODB2        ON  CACHE BOOL "ODB2 in metkit" )
 ecbuild_set_verbose( ENABLE_FDB         ON  CACHE BOOL "Both for FDB4 (client) and FDB5 (server)" )
 ecbuild_set_verbose( ENABLE_MIR_TOOLS   ON  CACHE BOOL "Install MIR tools" )
