beddb66744b5f64d0a4518efceaf552a700715bb
Metin Cakircali <metin.cakircali@ecmwf.int>
2025-09-24 16:52:34 +0200

    fix(CI/CD): add flags from metabuilder

--- cmake/ecmwf-hpc-nightly.cmake
+++ cmake/ecmwf-hpc-nightly.cmake
@@ -1,28 +1,30 @@
 ecbuild_set_verbose( ENABLE_MPI  OFF  CACHE BOOL "MPI" )
 
-### PYTHON
-
 ecbuild_set_verbose( ENABLE_PYTHON OFF CACHE BOOL "Python disabled" )
-
-### BUFR
-
-ecbuild_set_verbose( ENABLE_SINGLE_PRECISION OFF )
-
-ecbuild_set_verbose( BUFR_TABLES_PATH "/usr/local/apps/libemos/tables/bufr/text/000408" )
-
-### FDB
-
-ecbuild_set_verbose( ENABLE_ODBDIRECT   OFF  CACHE BOOL "Direct access to ODB on HPC" )
 
 ecbuild_set_verbose( ENABLE_FDB5        ON  CACHE BOOL "FDB5 enabled in client" )
 ecbuild_set_verbose( ENABLE_MIR_TOOLS   ON  CACHE BOOL "Install MIR tools" )
 
-ecbuild_set_verbose( FDB_CONF_FILE "/usr/local/apps/fdb/etc/Config" )
-
 ecbuild_set_verbose( ENABLE_EXPERIMENTAL  OFF   CACHE BOOL "experimental stuff" )
-ecbuild_set_verbose( ENABLE_FORTRAN       OFF   CACHE BOOL "fortran support" )
 ecbuild_set_verbose( ENABLE_AEC           ON    CACHE BOOL "eccodes AEC aka SZip encoding" )
 ecbuild_set_verbose( ENABLE_EIGEN         ON    CACHE BOOL "eigen support" )
-ecbuild_set_verbose( ENABLE_MEMFS         ON    CACHE BOOL "eccodes definitions in memory" )
 ecbuild_set_verbose( ENABLE_JPG           OFF   CACHE BOOL "eccodes JPG encoding" )
 ecbuild_set_verbose( ENABLE_FDB           OFF   CACHE BOOL "Both for FDB4 (client) and FDB5 (server)" )
+
+ecbuild_set_verbose( ENABLE_MEMFS            ON    CACHE BOOL "eccodes definitions in memory" )
+ecbuild_set_verbose( ENABLE_TESSELATION      OFF   CACHE BOOL " " )
+ecbuild_set_verbose( ENABLE_ODB2             ON    CACHE BOOL " " )
+ecbuild_set_verbose( ENABLE_BUFR             ON    CACHE BOOL " " )
+ecbuild_set_verbose( ENABLE_FDB              ON    CACHE BOOL " " )
+ecbuild_set_verbose( ENABLE_FFTW             ON    CACHE BOOL "for atlas support of SH transforms" )
+ecbuild_set_verbose( ENABLE_PROJ             ON    CACHE BOOL " " )
+ecbuild_set_verbose( ENABLE_ECKIT_GEO        OFF   CACHE BOOL " " )
+ecbuild_set_verbose( ENABLE_ECKIT_CODEC      OFF   CACHE BOOL " " )
+ecbuild_set_verbose( BUNDLE_SKIP_GRIBJUMP    OFF   CACHE BOOL " " )
+ecbuild_set_verbose( ENABLE_SINGLE_PRECISION OFF   CACHE BOOL "for libemos" )
+ecbuild_set_verbose( ECMWF                   ON    CACHE BOOL " " )
+
+ecbuild_set_verbose( BUNDLE_SKIP_FDB                ON CACHE BOOL   "disable FDB4" )
+ecbuild_set_verbose( BUNDLE_SKIP_LIBEMOS                ON CACHE BOOL "disable libemos" )
+ecbuild_set_verbose( ENABLE_PPROC_EMOS              OFF CACHE BOOL " " )
+ecbuild_set_verbose( ENABLE_RPC             OFF CACHE BOOL "RPC no longer needed" )
